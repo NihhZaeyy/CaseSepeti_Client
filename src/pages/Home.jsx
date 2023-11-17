f81415c4abf923dbb033ce1797ac6d74294df293
NihhZaeyy <nizaralmas11@gmail.com>
2023-11-17 22:47:39 +0700

    feat: navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Navbar from "../components/Navbar/Navbar";
+
 const Home = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="bg-primaryColor w-screen h-screen">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
